eab98c1916cff3f67fa6846c9c8ee00740d77979
Archanciel <jp.schnyder@gmail.com>
2018-09-22 18:47:38 +0200

    comment

--- explore_pygame.py
+++ explore_pygame.py
@@ -31,7 +31,7 @@ while run:
     if keys[pygame.K_DOWN]:
         y += velocity
 
-    win.fill((0, 0, 0))
+    win.fill((0, 0, 0)) #erasing previous rectangle
     pygame.draw.rect(win, (255, 0, 0), [x, y, width, height])
     pygame.display.update()
 pygame.quit()
